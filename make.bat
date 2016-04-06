e62f573158021bc76562a6cc283cbdd97f35ff10
Shun-ichi GOTO <shunichi.goto@gmail.com>
2016-04-06 10:54:15 +0900

    update make.bat to define SOCKLEN_T

--- make.bat
+++ make.bat
@@ -9,12 +9,13 @@ REM  cygwin ... Cygwin GNU C
 REM  mingw  ... MinGW GNU C
 
 REM set default copiler
+set DOPT=SOCKLEN_T=unsigned
 if x%COMPILER% == x set COMPILER=%1
 if x%COMPILER% == x set COMPILER=msc
-if %COMPILER%==msc     set CMD=cl /nologo connect.c wsock32.lib iphlpapi.lib advapi32.lib
-if %COMPILER%==bcc     set CMD=bcc32  connect.c wsock32.lib iphlpapi.lib advapi32.lib
-if %COMPILER%==cygwin  set CMD=gcc connect.c -o connect
-if %COMPILER%==mingw   set CMD=gcc connect.c -o connect -lwsock32 -liphlpapi
+if %COMPILER%==msc     set CMD=cl /nologo /D%DOPT%  connect.c wsock32.lib iphlpapi.lib advapi32.lib
+if %COMPILER%==bcc     set CMD=bcc32 /D%DOPT% connect.c wsock32.lib iphlpapi.lib advapi32.lib
+if %COMPILER%==cygwin  set CMD=gcc -D%DOPT% connect.c -o connect
+if %COMPILER%==mingw   set CMD=gcc -D%DOPT% connect.c -o connect -lwsock32 -liphlpapi
 
 echo %CMD%
 %CMD%
